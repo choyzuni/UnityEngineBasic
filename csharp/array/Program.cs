a2e0d432fcead08a819098176f681ffd6fd1d861
choyzuni <120104905+choyzuni@users.noreply.github.com>
2022-12-16 21:44:59 +0900

    12.16 C#

---
+++ csharp/array/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrint = new int[2];
+
+            arrint[0] = 375;
+            arrint[1] = 75;
+
+            int[] arrint_1 = { 1, 2, 3 };
+
+            int[][] arrarrint = new int[3][];
+
+            arrarrint[0] = new int[2];
+            arrarrint[1] = new int[2];
+            arrarrint[2] = new int[2];
+
+            arrarrint[0][0] = 35;
+
+            Console.WriteLine(arrarrint[0][0]);
+
+            string name = "choy";
+            char first = name[0];
+            Console.WriteLine(name[0]);
+            char[] arrchar = { 'k', 'i', 'm', '\0' };
+            Console.WriteLine(arrchar[0]);
+
+        }
+    }
+}
---
+++ csharp/loop_statements/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+
+namespace loop_statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = new int[100];
+            int ti = 0;
+            while (ti < 100)
+            {
+                arr[ti] = ti;
+                Console.WriteLine(arr[ti]);
+                ti++;
+            }
+
+            Console.WriteLine();
+
+            do
+            {
+                Console.WriteLine($"do while : {ti}");
+            } while (ti < 100);
+
+            Console.WriteLine();
+
+            string name = "choyjuniski";
+            for (ti = 0; ti < name.Length; ti++)
+            {
+                Console.WriteLine(name[ti]);
+            }
+
+            int[][] arrint = new int[3][];
+            arrint[0] = new int[3];
+            arrint[1] = new int[3];
+            arrint[2] = new int[3];
+
+            Console.WriteLine();
+
+            int tb = 0;
+
+            for (ti = 0; ti < arrint.Length; ti++)
+            {
+                for (int ta = 0; ta < arrint[ti].Length; ta++)
+                {
+                    arrint[ti][ta] = ++tb;
+                    Console.WriteLine(arrint[ti][ta]);
+                }
+            }
+
+        }
+    }
+}
