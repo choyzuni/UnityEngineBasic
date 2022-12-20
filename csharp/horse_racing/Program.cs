68fd1df5cfd6eee3b66cb783bb22eb2dd0075ff4
choyzuni <120104905+choyzuni@users.noreply.github.com>
2022-12-21 03:08:03 +0900

    12.20 C# + @

--- csharp/horse_racing/Program.cs
+++ csharp/horse_racing/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Threading;
 
 // 진행 방식
@@ -15,8 +16,6 @@ using System.Threading;
 //
 // 모든 말이 도착했다면 경주를 끝내고
 // 등수 순서대로 말들의 이름을 콘솔창에 출력한다
-// 
-
 
 namespace horse_racing
 {
@@ -25,90 +24,186 @@ namespace horse_racing
         class horse
         {
             public string name;
-            public int totaldistance = 0;
-            public int speed;
-            public bool finished;
+            private int totaldistance = 0;
+            public bool finished = false;
 
             public void run(int distance)
             {
-                this.totaldistance = this.totaldistance + distance;
+                if (goalpos <= totaldistance)
+                {
+                    if (finished == false && goalcheck < 5)
+                    {
+                        strings[goalcheck] = name;
+                        goalcheck++;
+                        finished = true;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+                else
+                {
+                    totaldistance += distance;
+                }
             }
 
+            public void display()
+            {
+                if (goalpos <= totaldistance)
+                {
+                    Console.WriteLine($"\t{name}  〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓 ||  " + "Finished !!");
+                }
+                else if (190 <= totaldistance)
+                {
+                    Console.WriteLine($"\t{name}  〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓▶ ||  " + totaldistance);
+                }
+                else if (180 <= totaldistance)
+                {
+                    Console.WriteLine($"\t{name}  〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓▶〓 ||  " + totaldistance);
+                }
+                else if (170 <= totaldistance)
+                {
+                    Console.WriteLine($"\t{name}  〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓▶〓〓 ||  " + totaldistance);
+                }
+                else if (160 <= totaldistance)
+                {
+                    Console.WriteLine($"\t{name}  〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓▶〓〓〓 ||  " + totaldistance);
+                }
+                else if (150 <= totaldistance)
+                {
+                    Console.WriteLine($"\t{name}  〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓▶〓〓〓〓 ||  " + totaldistance);
+                }
+                else if (140 <= totaldistance)
+                {
+                    Console.WriteLine($"\t{name}  〓〓〓〓〓〓〓〓〓〓〓〓〓〓▶〓〓〓〓〓 ||  " + totaldistance);
+                }
+                else if (130 <= totaldistance)
+                {
+                    Console.WriteLine($"\t{name}  〓〓〓〓〓〓〓〓〓〓〓〓〓▶〓〓〓〓〓〓 ||  " + totaldistance);
+                }
+                else if (120 <= totaldistance)
+                {
+                    Console.WriteLine($"\t{name}  〓〓〓〓〓〓〓〓〓〓〓〓▶〓〓〓〓〓〓〓 ||  " + totaldistance);
+                }
+                else if (110 <= totaldistance)
+                {
+                    Console.WriteLine($"\t{name}  〓〓〓〓〓〓〓〓〓〓〓▶〓〓〓〓〓〓〓〓 ||  " + totaldistance);
+                }
+                else if (100 <= totaldistance)
+                {
+                    Console.WriteLine($"\t{name}  〓〓〓〓〓〓〓〓〓〓▶〓〓〓〓〓〓〓〓〓 ||  " + totaldistance);
+                }
+                else if (90 <= totaldistance)
+                {
+                    Console.WriteLine($"\t{name}  〓〓〓〓〓〓〓〓〓▶〓〓〓〓〓〓〓〓〓〓 ||  " + totaldistance);
+                }
+                else if (80 <= totaldistance)
+                {
+                    Console.WriteLine($"\t{name}  〓〓〓〓〓〓〓〓▶〓〓〓〓〓〓〓〓〓〓〓 ||  " + totaldistance);
+                }
+                else if (70 <= totaldistance)
+                {
+                    Console.WriteLine($"\t{name}  〓〓〓〓〓〓〓▶〓〓〓〓〓〓〓〓〓〓〓〓 ||  " + totaldistance);
+                }
+                else if (60 <= totaldistance)
+                {
+                    Console.WriteLine($"\t{name}  〓〓〓〓〓〓▶〓〓〓〓〓〓〓〓〓〓〓〓〓 ||  " + totaldistance);
+                }
+                else if (50 <= totaldistance)
+                {
+                    Console.WriteLine($"\t{name}  〓〓〓〓〓▶〓〓〓〓〓〓〓〓〓〓〓〓〓〓 ||  " + totaldistance);
+                }
+                else if (40 <= totaldistance)
+                {
+                    Console.WriteLine($"\t{name}  〓〓〓〓▶〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓 ||  " + totaldistance);
+                }
+                else if (30 <= totaldistance)
+                {
+                    Console.WriteLine($"\t{name}  〓〓〓▶〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓 ||  " + totaldistance);
+                }
+                else if (20 <= totaldistance)
+                {
+                    Console.WriteLine($"\t{name}  〓〓▶〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓 ||  " + totaldistance);
+                }
+                else if (10 <= totaldistance)
+                {
+                    Console.WriteLine($"\t{name}  〓▶〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓 ||  " + totaldistance);
+                }
+                else if (0 <= totaldistance)
+                {
+                    Console.WriteLine($"\t{name}  ▶〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓 ||  " + totaldistance);
+                }
+            }
         }
 
         static Random random;
-        static int minspeed = 10;
-        static int maxspeed = 20;
+        static int minspeed = 1;
+        static int maxspeed = 30;
         static int goalpos = 200;
+        static int goalcheck = 0;
+        static string[] strings = new string[5];
+
+        static void gamerank()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("\t\t\t경기 종료!!!");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("\t\t     ★★★★★  1등 : " + strings[0]);
+            Console.WriteLine();
+            Console.WriteLine("\t\t      ★★★★  2등 : " + strings[1]);
+            Console.WriteLine();
+            Console.WriteLine("\t\t       ★★★  3등 : " + strings[2]);
+            Console.WriteLine();
+            Console.WriteLine("\t\t        ★★  4등 : " + strings[3]);
+            Console.WriteLine();
+            Console.WriteLine("\t\t         ★  5등 : " + strings[4]);
+            Console.WriteLine();
+            Console.WriteLine();
+        }
 
         static void Main(string[] args)
         {
             bool gamefinished = false;
-
-            // 말 5마리 만들고 이름 설정 ( 이름 패턴은 경주마 1,2,3,4~~~~ )
 
             horse[] horses = new horse[5];
 
             for (int ti = 0; ti < horses.Length; ti++)
             {
                 horses[ti] = new horse();
-                horses[ti].name = $"경주마 {ti + 1}";
+                horses[ti].name = $"{ti + 1}번마";
+                Console.WriteLine();
+                horses[ti].display();
             }
-
-            for (int ti = 0; ti < horses.Length; ti++)
-            {
-                Console.WriteLine(horses[ti].name);                
-            }
-
-
-
 
             while (gamefinished == false)
             {
-                
-
-                // 말 다섯마리 각각 아래처럼 랜덤한 스피드로 전진시키면서
-                // 각 말은 초당 10 ~ 20 ( 정수형 ) 범위의 거리를 랜덤하게 전진
-                // 각각의 말은 거리 200 에 도달하면 도착해서 더이상 전진하지 않고
-                // 매초 각 말들이 아직 달리고 있다면 달린 거리를 도착했다면 도착 상태를 콘솔창에 출력
-                // 모든 말이 도착했으면 게임 끝냄
-
-
                 random = new Random();
 
-                
-
-                
                 Thread.Sleep(1000); // 이 와일문은 1초에 한번씩 수행한다
 
                 Console.Clear();
-
 
                 for (int ti = 0; ti < horses.Length; ti++)
                 {
                     int tpspeed = random.Next(minspeed, maxspeed + 1);
                     horses[ti].run(tpspeed);
-                    Console.WriteLine($"경주마 {ti + 1} \t" + horses[ti].totaldistance);
+                    Console.WriteLine();
+                    horses[ti].display();
                 }
 
-
-                
-
-
-                
-                
+                if (horses[0].finished && horses[1].finished && horses[2].finished && horses[3].finished && horses[4].finished)
+                {
+                    gamefinished = true;
+                }
             }
 
             Console.Clear();
 
-
-            // 각 말들 등수 순서대로 이름 출력
+            gamerank();
         }
-
-        static void allrun()
-        { 
-        
-        }
-
     }
 }
